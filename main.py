0739af5de1c820c1ee00a3afd31cc8ca004b5683
YourAverageCoder69 <pfm251@gmail.com>
2020-06-06 11:18:19 +0500

    inital commit

---
+++ main.py
@@ -0,0 +1,114 @@
+import pygame
+import math
+from sudoku import *
+
+# board dimension constants
+BOARD_WIDTH: int = 405
+BOARD_HEIGHT: int = 405
+SCALE = 45  # the scale of box
+
+
+class Engine:
+    def __init__(self):
+        # init pygame stuff
+        pygame.init()
+        self.display: pygame.Surface = pygame.display.set_mode((BOARD_WIDTH, BOARD_HEIGHT))
+        pygame.display.set_caption("Sudoku Solver by haider_rauf")
+        self.clock: pygame.time = pygame.time.Clock()
+        self.font = pygame.font.SysFont("consolas", SCALE)
+
+        self.running = True  # game running status
+        self.current_selection = (0, 0)  # currently highlighted box
+        self.grid = [[0 for i in range(9)] for j in range(9)]  # init the grid
+
+    def handle_input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            elif event.type == pygame.KEYDOWN:
+                # pressing ENTER solves the sudoku
+                if event.key == pygame.K_RETURN:
+                    # first check if the sudoku is solvable...
+                    if can_solve(self.grid):
+                        solve_sudoku(self.grid)
+                        print("solved!")
+                    else:
+                        print("can't solve")
+
+                # move the target box via arrow keys
+                # update which row / col of sudoku is selected
+                # up / down means y-=1 / y+=1 and left / right means x-=1 /x+=1
+                elif event.key == pygame.K_UP:
+                    self.current_selection = (self.current_selection[0], self.current_selection[1] - 1)
+                elif event.key == pygame.K_DOWN:
+                    self.current_selection = (self.current_selection[0], self.current_selection[1] + 1)
+                elif event.key == pygame.K_LEFT:
+                    self.current_selection = (self.current_selection[0] - 1, self.current_selection[1])
+                elif event.key == pygame.K_RIGHT:
+                    self.current_selection = (self.current_selection[0] + 1, self.current_selection[1])
+
+                # SPACE key resets board (fill list with zeros i.e. empty)
+                elif event.key == pygame.K_SPACE:
+                    self.grid = [[0 for i in range(9)] for j in range(9)]
+
+                # enter number in grid
+                else:
+                    keys_pressed = pygame.key.get_pressed()  # get keyboard status
+                    (y, x) = self.current_selection
+                    for key in range(pygame.K_0, pygame.K_9 + 1):  # only do something if 0-9 keys are pressed
+                        if keys_pressed[key]:  # if any key number key is pressed...
+                            self.grid[y][x] = key - pygame.K_0   # calculate the grid value
+                            break
+
+    def update(self):
+        # logic so that the selection is warped
+        self.current_selection = (self.current_selection[0] % 10, self.current_selection[1] % 10)
+        self.clock.tick(30)
+
+    def render(self):
+        self.display.fill(pygame.color.Color(0, 0, 0, 255))
+        self.draw_grid((255, 255, 0, 255))
+
+        # draw numbers on grid
+        for i in range(BOARD_HEIGHT // SCALE):
+            for j in range(BOARD_WIDTH // SCALE):
+                # if current cell isn't empty...
+                if self.grid[i][j]:  # draw number here
+                    text_surf = self.font.render(str(self.grid[i][j]), True, (0, 0, 255, 255))
+                    self.display.blit(text_surf, (i * SCALE + 7, j * SCALE + 0))
+                # if current cell is selected, draw red box around it...
+                if (i, j) == self.current_selection:
+                    pygame.draw.rect(self.display, (255, 0, 0, 255), (i * SCALE, j * SCALE, SCALE, SCALE), 3)
+
+        pygame.display.flip()
+
+    def main_loop(self):
+        while self.running:
+            self.handle_input()
+            self.update()
+            self.render()
+
+    def draw_grid(self, color=(255, 255, 255, 255)):
+        # draw vertical lines
+        for i in range(1, BOARD_WIDTH // SCALE):
+            if i % 3:
+                pygame.draw.line(self.display, color, (i * SCALE, 0), (i * SCALE, BOARD_HEIGHT))
+            else:  # for the thick line for sub-grids
+                pygame.draw.line(self.display, color, (i * SCALE, 0), (i * SCALE, BOARD_HEIGHT), 5)
+
+        # draw horizontal lines
+        for i in range(1, BOARD_HEIGHT // SCALE):
+            if i % 3:
+                pygame.draw.line(self.display, color, (0, i * SCALE), (BOARD_WIDTH, i * SCALE))
+            else:  # for the thick line for sub-grids
+                pygame.draw.line(self.display, color, (0, i * SCALE), (BOARD_WIDTH, i * SCALE), 5)
+
+
+def main():
+    # init the engine and run the main loop
+    e = Engine()
+    e.main_loop()
+
+
+if __name__ == '__main__':
+    main()
---
+++ sudoku.py
@@ -0,0 +1,102 @@
+import copy
+
+
+def find_empty_cell(board: list):
+    # self-explanatory?
+    for y in range(9):
+        for x in range(9):
+            if board[y][x] == 0:
+                return x, y
+
+    return -1
+
+
+def print_board(board: list):
+    # print-board in console
+    print("    ", end="")
+    for i in range(9):
+        print(i, end="  ")
+
+    print("")
+    for i, row in enumerate(board):
+        print(i, "", row)
+
+
+# check if current board is solvable?
+def can_solve(board: list):
+    # ok so this algorithm looks messy because I wanted to reuse the is_possible() function instead of
+    # reinventing the wheel, fuk
+    for y in range(9):
+        for x in range(9):
+            if board[y][x] != 0:
+                result = None
+                val = board[y][x]  # store value at cell
+                board[y][x] = 0  # clear the cell
+                if is_possible(board, val, x, y):  # check if value at cell would be valid
+                    result = True
+                else:
+                    result = False
+                board[y][x] = val  # restore the cell's value
+
+                if not result:  # if that value would've been illegal, return fase
+                    return False
+
+    return True
+
+
+# check if putting val at (x, y) cell is possible
+def is_possible(board: list, val: int, x: int, y: int):
+    # check row for val
+    for row in range(9):
+        if board[row][x] == val:
+            return False
+
+    # check column
+    for col in range(9):
+        if board[y][col] == val:
+            return False
+
+    # calculate offsets for sub-grid
+    # a neat little trick exploiting truncating division, I hate that Python uses // for this
+    x_off = (x // 3) * 3
+    y_off = (y // 3) * 3
+
+    # finally check the sub-grid
+    for y in range(3):
+        for x in range(3):
+            if board[y_off + y][x_off + x] == val:
+                return False
+
+    return True
+
+
+# Ok, this is the main sudoku solving algorithm
+# It's a well-known technique known as Backtracking, you can easily find
+# explanations about this on the internet, way better than I can explain.
+# So I'm not gonna go into theory, but I STRONGLY suggest you look into it,
+# it's a good way to see an actual practical use of recursion instead
+# of some some factorial bullshit (who the hell uses that irl?)
+def solve_sudoku(board: list):
+    # find current empty position...
+    pos = find_empty_cell(board)
+
+    # if there is no empty cell, YAY! board is solved!
+    if pos == -1:
+        return True
+
+    # ...else get empty cell position
+    (x, y) = pos
+
+    for val in range(1, 10):
+        # check each value for possibility...
+        if is_possible(board, val, x, y):
+            # ...if possible, set the value
+            board[y][x] = val
+            # ...recursively call the function again
+            if solve_sudoku(board):
+                return True
+            board[y][x] = 0
+
+    # if no value is valid at position, return False, go to previous stack frame..
+    # hence triggering backtracking...
+    return False
